1d90a1ef19777cabb8dfc274bf5eaa2ce65b0864
ankitsaini009 <ankitsaini952005@gmail.com>
2026-09-08 15:35:38 +0530

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/breathWell-pulmonary-clinic/',
+
   build: {
     rollupOptions: {
       input: {
